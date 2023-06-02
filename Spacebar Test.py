c34c7855492f9f338d5dde8a2579e8e3ea453dea
Leo Larocque <117418726+LeoLarocque@users.noreply.github.com>
2023-06-02 13:05:53 -0600

    Add files via upload

---
+++ Spacebar Test.py
@@ -0,0 +1,5 @@
+import pyautogui as py
+import time
+time.sleep(5)
+for i in range(1, 500):
+    py.write("                                                                                                                                                                                                                              ")
---
+++ Typing Test Cheat.py
@@ -0,0 +1,5 @@
+#powertyping.com/typing_test/typing_test.html
+import pyautogui as py
+import time
+time.sleep(5)
+py.write('Presented are online free timed typing tests to test your typing speed. The tests topic is a humorous literally work of Jeremy K Jeremy, Three Men in a Boat ( To say nothing of the Dog ); a novel praised not so much for its linguistic value or the extent and usefulness of information it conveys, but rather for its simple truthfulness. Tests follow the novel and are updated on monthly bases. Typing speed is measured in wpm, words typed per minute, and a word is defined as five characters including blank spaces. Typing speed is also measured in cpm - characters typed per minute. Again, it includes typing blank spaces. Click a link below to load different text, click the "start test" button and begin typing.')
